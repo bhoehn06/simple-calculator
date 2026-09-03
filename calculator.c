455469c458b1026ff8a302b3088c77c36948aa0c
Benjamin Hoehn <benjamin.hoehn@siu.edu>
2026-09-03 10:54:41 -0500

    Implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+double add(double a, double b);
+
 int main(){
-    
+    double x= 5.0;
+    double y= 5.0;
+    double test1=add(x,y);
+    printf("%lf\n", test1);
+}
+
+double add(double a, double b){
+    return a+b;
 }
