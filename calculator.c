a66eecb20317c80afff4d6e6a01bd21db0248238
Benjamin Hoehn <benjamin.hoehn@siu.edu>
2026-09-03 11:32:27 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -4,6 +4,7 @@
 double add(double a, double b);
 double subtract(double a, double b);
 double multiply(double a, double b);
+double divide(double a, double b);
 
 int main(){
     //Defines the testing variables
@@ -14,9 +15,13 @@ int main(){
     double test1=add(x,y);
     double test2=subtract(x,y);
     double test3=multiply(x,y);
+    double test4=divide(x,y);
+
+    //Prints the values of the test functions
     printf("%lf\n", test1);
     printf("%lf\n", test2);
-    printf("%lf",test3);
+    printf("%lf\n", test3);
+    printf("%lf\n", test4);
 }
 
 //Defines the calculator functions
@@ -30,4 +35,13 @@ double subtract(double a, double b){
 
 double multiply(double a, double b){
     return a*b;
+}
+
+double divide(double a, double b){
+   //Protects program against divide by zero errors
+    if (b==0.0){
+        printf("Error, division by zero is not allowed.\n");
+        return 0.0; //Returns to default value
+    }
+    return a/b;
 }
