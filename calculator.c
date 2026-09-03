25608d574c38b27bdbfa05662c2eea7ec5e9ee75
Benjamin Hoehn <benjamin.hoehn@siu.edu>
2026-09-03 12:23:02 -0500

    Added comments and documentation

--- calculator.c
+++ calculator.c
@@ -12,36 +12,34 @@ int main(){
     double z;
     char operation;
 
+    //Prompts the user to select an operator from the options available
     printf("Please choose an operator:\n Addition: A\n Subtraction: S\n Multiplication: M\n Division: D\n");
     
-    //
+    //Checks to see if the value entered by the user is a character, and terminates the program if it isn't
     if (scanf("%c", &operation)!=1){
         printf("Invalid input, terminating program.\n");
         return 1;
     }
 
-    //
-    
+    //Checks to see if the user entered a valid character, and terminates the program if they entered an invalid character
     if((operation!='A')&&(operation!='S')&&(operation!='M')&&(operation!='D')){
         printf("Invalid input, terminating program.\n");
         return 1;
     }
 
-    //
-    printf("Please input a value for x:\n");
-    if(scanf("%lf", &x)!=1){
+    printf("Please input a value for x:\n");//Prompts the user to input a value for x
+    if(scanf("%lf", &x)!=1){ //Checks if the value for x is of type double, and terminates the program otherwise.
         printf("Invalid input, terminating program.\n");
         return 1;
     }
 
-    //
-    printf("Please input a value for y:\n");
-    if(scanf("%lf", &y)!=1){
+    printf("Please input a value for y:\n");    //Prompts the user to input a value for y
+    if(scanf("%lf", &y)!=1){//Checks if the value for y is of type double and terminates the program otherwise.
         printf("Invalid input, terminating program.\n");
         return 1;
     }
 
-
+    //Determines which operation the user wanted, then executes it and outputs it to the user
     if(operation == 'A'){
         z=add(x,y);
         printf("%lf plus %lf equals: %lf\n", x,y,z);
@@ -52,9 +50,9 @@ int main(){
         z=multiply(x,y);
         printf("%lf times %lf equals: %lf\n", x,y,z);
     }else if(operation=='D'){
-         if (y==0.0){
+         if (y==0.0){ //Checks if the user gave a value of zero for y, and ends the program if so.
         printf("Error, division by zero is not allowed.\n");
-        return 0.0; //Returns to default value
+        return 0.0; 
     }
         z=divide(x,y);
         printf("%lf divided by %lf equals: %lf\n", x,y,z);
@@ -67,15 +65,12 @@ int main(){
 double add(double a, double b){
     return a+b;
 }
-
 double subtract(double a, double b){
     return a-b;
 }
-
 double multiply(double a, double b){
     return a*b;
 }
-
 double divide(double a, double b){
     return a/b;
 }
