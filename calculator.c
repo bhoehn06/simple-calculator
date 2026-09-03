ca033a5abec0e733da2b59465c6941c3bc0451a9
Benjamin Hoehn <benjamin.hoehn@siu.edu>
2026-09-03 11:20:48 -0500

    Implemented subtraction feature

--- calculator.c
+++ calculator.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 double add(double a, double b);
+double subtract(double a, double b);
 
 int main(){
     double x= 5.0;
-    double y= 5.0;
+    double y= 10.0;
     double test1=add(x,y);
+    double test2=subtract(x,y);
     printf("%lf\n", test1);
+    printf("%lf\n", test2);
 }
 
 double add(double a, double b){
     return a+b;
+}
+
+double subtract(double a, double b){
+    return a-b;
 }
